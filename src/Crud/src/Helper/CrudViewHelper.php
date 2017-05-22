a5d42a1e983e7fccec9079626fddab4f4b105647
Rostislav <rostmefpoter@gmail.com>
2017-05-22 17:36:31 +0300

    first commit

---
+++ src/Crud/src/Helper/CrudViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace rollun\Crud\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Class CrudViewHelper
+ * @package rollun\test\Helper
+ */
+class CrudViewHelper extends AbstractHelper
+{
+
+
+	protected static $_initialized = false;
+
+	/**
+	 * @param $url
+	 * @param null $title
+	 * @param array $options
+	 * @return string
+	 */
+	public function __invoke($url, $title = null, array $options = [])
+	{
+		$crud_uid = isset($options['crud_uid']) ? $options['crud_uid'] : 'crud_' . rand(1, 10000);
+		$options = json_encode($options);
+		$this->assets($crud_uid);
+		return "<div id='{$crud_uid}'>\n<w-crud-app title='{$title}' url='{$url}' options='{$options}'>\n</w-crud-app>\n</div>";
+	}
+
+	/**
+	 *
+	 */
+	public function assets($crud_uid)
+	{
+		$view = $this->getView();
+		if (!static::$_initialized) {
+			$view->headLink()
+				->appendStylesheet('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+			$view->inlineScript()
+				->appendFile('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js')
+				->appendFile('https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js');
+
+			$view->bootstrap();
+
+			$view->inlineScript()
+				->appendScript("dojoConfig = {async: true,isDebug: true,packages: [{name: 'dstore', location: '/assets/js/dojo-dstore/'}, {name: 'rql',location: '/assets/js/rql/'}]};");
+
+			$view->inlineScript()
+				->appendFile("/assets/js/dojo/dojo.js")
+				->appendFile("/assets/js/rollun-js/index.js");
+			static::$_initialized = true;
+		}
+		$view->inlineScript()
+			->appendScript("$(function () {require(['dojo/_base/declare','dstore/Rest','dstore/extensions/RqlQuery','rql/query'], function (declare, Rest, RqlQuery, query) {window.RqlStore = declare([Rest, RqlQuery]);Query = query.Query;RollunJs.app({el: '#{$crud_uid}'});});});");
+	}
+}
---
+++ src/Crud/src/Middleware/CrudAction.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victorsecuring
+ * Date: 06.05.17
+ * Time: 11:33 AM
+ */
+
+namespace rollun\Crud\Middleware;
+
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use rollun\actionrender\Renderer\Html\HtmlParamResolver;
+
+class CrudAction implements MiddlewareInterface
+{
+
+	/**
+	 * Process an incoming server request and return a response, optionally delegating
+	 * to the next middleware component to create the response.
+	 *
+	 * @param ServerRequestInterface $request
+	 * @param DelegateInterface $delegate
+	 *
+	 * @return ResponseInterface
+	 */
+	public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+	{
+		$params = $request->getQueryParams();
+		$params = array_merge($params, [
+			// можно все передать через параметры урла
+			'url' => '/api/datastore/test',
+			'title' => 'test table',
+			'options' => [
+
+			]
+		]);
+		$request = $request->withAttribute('responseData', $params);
+		$request = $request->withAttribute(HtmlParamResolver::KEY_ATTRIBUTE_TEMPLATE_NAME, 'crud-app::home-page');
+		$response = $delegate->process($request);
+
+		return $response;
+	}
+}
