f305a652fb6ba577cde3230d1cecb82a1095eb6e
bryakwork <bryakwork@gmail.com>
2018-01-05 14:05:27 +0200

    updated for new importer features

--- src/Crud/src/Helper/ImporterViewHelper.php
+++ src/Crud/src/Helper/ImporterViewHelper.php
@@ -6,28 +6,32 @@
  * Time: 15:32
  */
 
-namespace rollun\Crud\Helper;
+namespace rollun\amazonItemSearch\visual;
 
 use Zend\View\Helper\AbstractHelper;
 
 class ImporterViewHelper extends AbstractHelper
 {
     /**
-     * @param bool $noIdMode
-     * @param string $popupButtonLabel
-     * @param string $popupTitle
-     * @param string $placeholderText
+     * @param array $options
      * @return string
      */
-    function __invoke($noIdMode = false,
-                      $popupButtonLabel = "Добавить записи",
-                      $popupTitle = "Добавление записей",
-                      $placeholderText = "Введите новые данные")
+    function __invoke($options)
     {
+        $importFields = (isset($options["importFields"]) ? json_encode($options["importFields"]) : json_encode(null));
+        $noIdMode = (isset($options["noId"]) ? $options["noId"] : '0');
+        $popupButtonLabel = (isset($options["popupButtonLabel"]) ? $options["popupButtonLabel"] : "Add items");
+        $popupTitle = (isset($options["popupTitle"]) ? $options["popupTitle"] : "Add new items");
+        $placeholderText = (isset($options["placeholderText"]) ? $options["placeholderText"] : "Enter new items here");
         $view = $this->getView();
         $view->inlineScript()->appendScript("$(function () {importerApp = RollunJs.app({el: '#crud_importer'});});");
         return "<div id=\"crud_importer\">
-                    <w-crud-import label='{$popupButtonLabel}' noid='{$noIdMode}' popuptitle='{$popupTitle}' placeholder='{$placeholderText}'></w-crud-import>
+                    <w-crud-import
+                        importfields='{$importFields}' 
+                        label='{$popupButtonLabel}' 
+                        noid='{$noIdMode}' 
+                        popuptitle='{$popupTitle}' 
+                        placeholder='{$placeholderText}'></w-crud-import>
                 </div>";
     }
 }
