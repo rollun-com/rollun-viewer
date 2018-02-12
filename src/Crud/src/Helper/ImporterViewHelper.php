07a834fb92619725619a33a631c44098b0486488
bryakwork <bryakwork@gmail.com>
2018-02-12 18:37:45 +0200

    Updated ImporterViewHelper for new features

--- src/Crud/src/Helper/ImporterViewHelper.php
+++ src/Crud/src/Helper/ImporterViewHelper.php
@@ -20,10 +20,14 @@ class ImporterViewHelper extends AbstractHelper
     {
         $importFields = (isset($options["importFields"]) ? json_encode($options["importFields"]) : json_encode(null));
         $noIdMode = (isset($options["noId"]) ? json_encode($options["noId"]) : json_encode(false));
-        $inputSeparator = (isset($options["inputSeparator"]) ? $options["inputSeparator"] : "   ");
+        $inputSeparator = (isset($options["inputSeparator"]) ? $options["inputSeparator"] : "\t");
         $popupButtonLabel = (isset($options["popupButtonLabel"]) ? $options["popupButtonLabel"] : "Add items");
         $popupTitle = (isset($options["popupTitle"]) ? $options["popupTitle"] : "Add new items");
         $placeholderText = (isset($options["placeholderText"]) ? $options["placeholderText"] : "Enter new items here");
+        $formName = (isset($options["formName"]) ? $options["formName"] : 'file2ds');
+        $uploadUrl = json_encode($options["uploadUrl"]);
+        $uploadAccept = (isset($options["uploadAccept"]) ? json_encode($options["uploadAccept"]) : '.csv');
+        $uploadHeaders = (isset($options["uploadHeaders"]) ? json_encode($options["uploadHeaders"]) : '');
 
         $view = $this->getView();
         $view->bootstrap();
@@ -31,12 +35,17 @@ class ImporterViewHelper extends AbstractHelper
 
         return "<div id=\"crud_importer\">
                     <w-crud-import
-                        importfields='{$importFields}' 
-                        label='{$popupButtonLabel}' 
-                        noid='{$noIdMode}' 
-                        inputSeparator='{$inputSeparator}'
-                        popuptitle='{$popupTitle}' 
-                        placeholder='{$placeholderText}'></w-crud-import>
+                        importfields='$importFields' 
+                        label='$popupButtonLabel' 
+                        noid='$noIdMode' 
+                        inputSeparator='$inputSeparator'
+                        placeholder='$placeholderText'
+                        popuptitle='$popupTitle' 
+                        formname='$formName'
+                        uploadurl='$uploadUrl'
+                        uploadaccept='$uploadAccept'
+                        uploadheaders='$uploadHeaders'>
+                    </w-crud-import>
                 </div>";
     }
 }
