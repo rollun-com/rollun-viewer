1375323bfaf27540491b7c9b2e446429863e8d46
victorynox <it.professor02@gmail.com>
2018-03-29 16:45:49 +0300

    Refactor to new expressive version. Remove deprecated features .

--- config/config.php
+++ config/config.php
@@ -12,11 +12,9 @@ $cacheConfig = [
 
 $aggregator = new ConfigAggregator([
     \Zend\I18n\ConfigProvider::class,
-    \Zend\Db\ConfigProvider::class,
     \Zend\Validator\ConfigProvider::class,
-    \Zend\Filter\ConfigProvider::class,
-    \Zend\Cache\ConfigProvider::class,
-    \Zend\Session\ConfigProvider::class,
+    \Zend\Expressive\ConfigProvider::class,
+    \Zend\Expressive\Router\ConfigProvider::class,
     \rollun\Crud\ConfigProvider::class,
     \rollun\actionrender\ConfigProvider::class,
     // Include cache configuration
