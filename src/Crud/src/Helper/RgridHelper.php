d8fe551eebd7efa8fb6c340ab74506cae2da7904
bryakwork <bryakwork@gmail.com>
2018-06-19 11:46:20 +0300

    Changes for rgrid 0.2

--- src/Crud/src/Helper/RgridHelper.php
+++ src/Crud/src/Helper/RgridHelper.php
@@ -21,25 +21,58 @@ class RgridHelper extends AbstractHelper
         $this->addDojoStyles($view);
         $dataViewerHtml = "
             <script>
-                require([
-                    \"dojo/dom\",
-                    \"dojo/dom-class\",
-                    \"dojo/_base/window\",
-                    \"Rscript/DataStoreViewer/widget/AdvDatastoreViewer\",
-                    'dstore/Memory',
-                ], function (dom,
-                             domClass,
-                             win,
-                             AdvDatastoreViewer,
-                             Memory) {
-                    
-                    domClass.add(win.body(), \"flat\");
-                    var myFilterStore = new Memory();
-                    var dataStoreViewer = new AdvDatastoreViewer({url: '$url', gridConfig: {filterStore: myFilterStore}});
-                    dataStoreViewer.placeAt(dom.byId('data_store_viewer')).startup();
-                })
+                require(
+                    [
+                        'dojo/dom',
+                        'dojo/dom-class',
+                        'dojo/_base/window',
+                        'rgrid/Composite/RComposite',
+                        'rgrid/Composite/WidgetFactory',
+                        'rgrid/Composite/TemplateWidgetPlacer',
+                        'rgrid/prefabs/ConditionsInMenu',
+                        'rgrid/prefabs/Pagination',
+                        'rgrid/prefabs/Rgrid',
+                        'rgrid/prefabs/Search',
+                        'dstore/Memory',
+                        'dojo/text!rgrid/testTemplate.html'
+                    ], function (
+                        dom,
+                        domClass,
+                        win,
+                        RComposite,
+                        WidgetFactory,
+                        WidgetPlacer,
+                        ConditionsInMenu,
+                        PaginationPrefab,
+                        RgridPrefab,
+                        SearchPrefab,
+                        Memory,
+                        template                   
+                        ) {
+                        domClass.add(win.body(), 'flat');
+                        const factory = new WidgetFactory(),
+                        placer = new WidgetPlacer(),
+                        configStore = new Memory({data: [{id: 'gridTarget', url: \"$url\"}]}),
+                        composite = new RComposite({
+                            widgetFactory: factory, 
+                            widgetPlacer: placer, 
+                            configStore: configStore,
+                            templateString: template
+                        }),
+                        prefabs = [
+                        	new RgridPrefab(),
+                        	new ConditionsInMenu(),
+                        	new PaginationPrefab(),
+                        	new SearchPrefab()
+                        	];
+                        composite.addComponents(prefabs);
+                        composite.placeAt(dom.byId('data_store_viewer'));
+                        composite.startup();
+                        }
+);
+                      
             </script>
-            <div id=\"data_store_viewer\" ></div>
+            <div id=\"data_store_viewer\"></div>
         ";
 
         return $dataViewerHtml;
@@ -48,17 +81,14 @@ class RgridHelper extends AbstractHelper
     protected function addDojoStyles($view)
     {
         $view->headLink()
-            ->appendStylesheet('https://ajax.googleapis.com/ajax/libs/dojo/1.11.2/dojo/resources/dojo.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.0.x/themes/flat/flat.css')
+            ->appendStylesheet('https://ajax.googleapis.com/ajax/libs/dojo/1.11.1/dojo/resources/dojo.css')
+            ->appendStylesheet('https://ajax.googleapis.com/ajax/libs/dojo/1.11.1/dojo/resources/dnd.css')
+            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.x/themes/flat/flat.css')
+            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.x/lib/css/rgrid.css')
+            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.x/lib/css/ConditionEditor.css')
             ->appendStylesheet('https://cdn.jsdelivr.net/npm/dojox@1.x/highlight/resources/highlight.css')
             ->appendStylesheet('https://cdn.jsdelivr.net/npm/dojox@1.x/highlight/resources/pygments/colorful.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/dgrid@1.x/css/dgrid.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/dojox@1.x/grid/enhanced/resources/EnhancedGrid_rtl.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.0.x/lib/FilterEditor/resources/css/FilterEditor.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.0.x/lib/DataStoreViewer/resource/bootstrap/css/bootstrap.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.0.x/lib/DataStoreViewer/resource/bootstrap/css/bootstrap-theme.css')
-            ->appendStylesheet('https://ajax.googleapis.com/ajax/libs/dojo/1.11.2/dojo/resources/dnd.css')
-            ->appendStylesheet('https://cdn.jsdelivr.net/npm/rgrid@0.0.x/lib/DataStoreViewer/resource/bootstrap/css/dashboard.css');
+            ->appendStylesheet('https://cdn.jsdelivr.net/npm/dgrid@1.x/css/dgrid.css');
     }
 
     protected function addDojo($view)
@@ -69,36 +99,36 @@ class RgridHelper extends AbstractHelper
                 isDebug: true,
                 packages: [
                     {
-                        name: \"Rscript\",
-                        location: 'https://cdn.jsdelivr.net/npm/rgrid@0.0.x/lib'
+                        name: 'rgrid',
+                        location: 'https://cdn.jsdelivr.net/npm/rgrid@0.x/lib'
                     },
                     {
-                        name: \"dstore\",
+                        name: 'dstore',
                         location: 'https://cdn.jsdelivr.net/npm/dojo-dstore@1.x'
                     }, 
                     {
-                        name: \"dgrid\",
+                        name: 'dgrid',
                         location: 'https://cdn.jsdelivr.net/npm/dgrid@1.x'
                     }, 
                     {
-                        name: \"dijit\",
+                        name: 'dijit',
                         location: 'https://cdn.jsdelivr.net/npm/dijit@1.x'
                     }, 
                     {
-                        name: \"dojox\",
+                        name: 'dojox',
                         location: 'https://cdn.jsdelivr.net/npm/dojox@1.x'
                     }, 
                     {
-                        name: \"promised-io\",
+                        name: 'promised-io',
                         location: 'https://cdn.jsdelivr.net/npm/promised-io@0.x'
                     }, 
                     {
-                        name: \"rql\",
+                        name: 'rql',
                         location: 'https://cdn.jsdelivr.net/npm/rollun-rql@0.x'
                     }
                 ]
             };
         ");
-        $view->inlineScript()->appendFile("https://ajax.googleapis.com/ajax/libs/dojo/1.11.2/dojo/dojo.js");
+        $view->inlineScript()->appendFile("https://ajax.googleapis.com/ajax/libs/dojo/1.11.1/dojo/dojo.js");
     }
 }
